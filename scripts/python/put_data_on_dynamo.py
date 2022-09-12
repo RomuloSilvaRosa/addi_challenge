79e213755aed0a33e727be6d97c0e74361a10df5
Romulo <you@example.com>
2022-09-12 12:05:40 -0300

    Fix put on dynamo helper script paths

--- scripts/python/put_data_on_dynamo.py
+++ scripts/python/put_data_on_dynamo.py
@@ -2,13 +2,13 @@ import sys
 
 import pandas as pd
 
-sys.path.append("./codes/model_orchestrator")
+sys.path.append("./lambdas/model_orchestrator")
 from src.gateways.aws.dynamodb.credit_card_feature_store import \
     CreditCardFeatureStoreGateway
 
 
 def put_data(indexes):
-    df = pd.read_csv("./codes/model_trainning/data/UCI_Credit_Card.csv")
+    df = pd.read_csv("./lambdas/model_trainning/data/UCI_Credit_Card.csv")
 
     df.set_index("ID", inplace=True)
     df.rename(columns={"PAY_0": "PAY_1"}, inplace=True)
@@ -17,10 +17,10 @@ def put_data(indexes):
     values = features.iloc[indexes]
     values_dict = values.reset_index().to_dict(orient='records')
     for x in values_dict:
-        x_id = x.pop("ID")
         print(x)
+        x_id = x.pop("ID")
         CreditCardFeatureStoreGateway.insert_data(x_id, x)
 
 
-INDEXES = [0, 1]
+INDEXES = [0, 1, 2]
 put_data(INDEXES)
