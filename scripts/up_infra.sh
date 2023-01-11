84397c6358ce1d8fadbabf8d7d3a4271b9735ff3
romulo <romulo@romulos-MacBook-Pro.local>
2023-01-11 07:29:29 -0300

    Add requirements-dev

--- scripts/up_infra.sh
+++ scripts/up_infra.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-# bash scripts/CD.sh -a
-# bash scripts/CI.sh
+python3 -m pip install -r requirements-dev.txt
+bash scripts/CD.sh -a
+bash scripts/CI.sh
 
 aws athena start-query-execution --query-string "$(cat scripts/evaluation_store.sql | tr '\n' ' ')"
